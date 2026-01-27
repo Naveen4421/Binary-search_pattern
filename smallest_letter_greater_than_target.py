feff6f7871903890df76e427a7e0fdaa551b59e7
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-01-27 21:15:54 +0530

    Add function to find next greatest letter
    Implement binary search to find the smallest letter greater than the target.

---
+++ smallest_letter_greater_than_target.py
@@ -0,0 +1,14 @@
+def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        l,r=0,len(letters)-1
+        while l<=r:
+            mid = (l+r)//2
+            if letters[mid]<=target:
+                l = mid+1
+            else:
+                r = mid-1
+        return letters[l%len(letters)]
