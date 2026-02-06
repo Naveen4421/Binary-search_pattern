9e10d201be2ab4c4ca8619430885336d9d9603ec
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-02-06 20:16:43 +0530

    Implement binary search for first bad version

---
+++ first_bad_version.py
@@ -0,0 +1,10 @@
+low,high=0,n
+        result=n
+        while low<=high:
+            mid=(low+high)/2
+            if (isBadVersion(mid)):
+                result=mid
+                high =mid-1
+            else:
+                low = mid+1
+        return result
