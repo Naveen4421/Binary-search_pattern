78bf9f6e85d4d77886d5079f22c479d053303d9a
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-02-18 21:48:42 +0530

    Implement h-index calculation using binary search

---
+++ H-index.py
@@ -0,0 +1,20 @@
+def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        left, right = 0, n - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            h = n - mid
+            
+            if citations[mid] == h:
+                return h
+            elif citations[mid] < h:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return n - left
